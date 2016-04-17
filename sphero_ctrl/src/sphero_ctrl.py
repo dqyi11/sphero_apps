7d97d10e906faf217a01280f1e32ed8b9acfa1c6
walter <daqing.yi@byu.edu>
2016-04-17 11:34:21 -0600

    fix pd controller

--- sphero_ctrl/src/sphero_ctrl.py
+++ sphero_ctrl/src/sphero_ctrl.py
@@ -45,11 +45,22 @@ class SpheroCtrl:
     def sphero_pos_callback(self, msg):
         if self.runnable == True:
             current_pos = [float(msg.x), float(msg.y)]
-            #print "current pos : " + str(current_pos)
-            if self.sphero_target_pos[0] >= 0 and self.sphero_target_pos[1] >= 0:
+
+            if current_pos[0] < 0 or current_pos[1] < 0:
+                # stop when running out of view
+                cv = Twist()
+                cv.linear.y = 0.0
+                cv.linear.y = 0.0
+                cv.linear.z = 0.0
+                cv.angular.x = 0.0
+                cv.angular.y = 0.0
+                cv.angular.z = 0.0
+                self.cmd_vel_pub.publish(cv)
+              
+            elif self.sphero_target_pos[0] >= 0 and self.sphero_target_pos[1] >= 0:
                 delta_x = self.sphero_target_pos[0] - current_pos[0]
                 delta_y = self.sphero_target_pos[1] - current_pos[1]
-                #print "delta pos : " + str([delta_x, delta_y])
+
                 d_delta_x = 0.0
                 d_delta_y = 0.0          
                 if self.last_time == None:
@@ -59,32 +70,37 @@ class SpheroCtrl:
                 else:
                    new_time = rospy.get_time()
                    delta_t = new_time - self.last_time
-                   delta_x = current_pos[0] - self.last_x 
-                   delta_y = current_pos[1] - self.last_y
+                   delta_last_x = current_pos[0] - self.last_x 
+                   delta_last_y = current_pos[1] - self.last_y
                    self.last_time = new_time
                    self.last_x = current_pos[0]
                    self.last_y = current_pos[1]
-                   d_gain_x = delta_x / delta_t
-                   d_gain_y = delta_y / delta_t
-                   print "d_delta " + str([d_gain_x, d_gain_y])
+                   d_delta_last_x = delta_last_x / delta_t
+                   d_delta_last_y = delta_last_y / delta_t
 
-                gain_x = self.Kp * delta_x + self.Kd * d_delta_x
-                gain_y = self.Kp * delta_y + self.Kd * d_delta_y
+                gain_x = self.Kp * delta_x + self.Kd * d_delta_last_x
+                gain_y = self.Kp * delta_y + self.Kd * d_delta_last_y
  
                 cv = Twist()
-                if delta_x > 0: 
-                    cv.linear.x = min(- gain_x , -10.0)
-                elif delta_x < 0:
-                    cv.linear.x = max(- gain_x , 10.0)
-                if delta_y > 0:
-                    cv.linear.y = max( gain_y, 10.0)
-                elif delta_y < 0:
-                    cv.linear.y = min( gain_y, -10.0)
+                if gain_x > 0: 
+                    cv.linear.x = min(- gain_x , -20.0)
+                elif gain_x < 0:
+                    cv.linear.x = max(- gain_x , 20.0)
+                if gain_y > 0:
+                    cv.linear.y = max( gain_y, 20.0)
+                elif gain_y < 0:
+                    cv.linear.y = min( gain_y, -20.0)
                 cv.linear.z = 0.0
                 cv.angular.x = 0.0
                 cv.angular.y = 0.0
                 cv.angular.z = 0.0
-                print "vel : " + str([cv.linear.x, cv.linear.y])             
+                if cv.linear.x != 0.0 and cv.linear.y != 0.0:
+                    #print "tar : " + str(self.sphero_target_pos)
+                    #print "curr : " + str(current_pos)
+                    #print "del : " + str([delta_x, delta_y])
+                    print "d_t : " + str(delta_t)
+                    print "d_del : " + str([d_delta_last_x, d_delta_last_y])
+                    print "vel : " + str([cv.linear.x, cv.linear.y])           
                 self.cmd_vel_pub.publish(cv)
                 
 
